bbad084f3bf58604519719dda37200ee549cad15
vg0x00 <vg0x00@gmail.com>
2017-08-31 19:41:33 +0800

    init emacs config and theme

---
+++ emacs/themes/wu-theme.el
@@ -0,0 +1,11 @@
+(deftheme wu)
+
+(custom-theme-set-faces
+ 'wu
+ '(default ((t (:background "#ffffff" :foreground "#000000"))))
+ '(font-lock-comment-face ((t (:foreground "#999999"))))
+ '(hl-line ((t (:background "#cccccc"))))
+ '(region ((t (:background "#ffcccc"))))
+)
+
+(provide-theme 'wu)
