dc52a30bcd6ddb24da66558ea38c78b22fb93f2f
vg0x00 <vg0x00@gmail.com>
2017-11-08 12:29:29 +0800

    divide theme config into sections

--- emacs/themes/wu-theme.el
+++ emacs/themes/wu-theme.el
@@ -3,9 +3,9 @@
 (custom-theme-set-faces
  'wu
  '(default ((t (:family "Fira Code" :background "#121212" :foreground "#93a1a1"))))
+ 
+ ;; build in syntax
  '(font-lock-comment-face ((t (:foreground "#7a8b8b"))))
- '(highlight ((t (:background "#4a708b"))))
- '(region ((t (:background "#cdb7b5"))))
  '(font-lock-string-face ((t (:foreground "#2aa198" :weight normal))))
  '(font-lock-keyword-face ((t (:foreground "#9AB77C" :weight normal))))
  '(font-lock-function-name-face ((t (:foreground "#ffb5c5" :weight normal))))
@@ -15,8 +15,16 @@
  '(font-lock-variable-name-face ((t (:foreground "#cdaa7d" :weight normal))))
  '(font-lock-type-face ((t (:foreground "#96cdcd" :weight normal))))
  '(font-lock-preprocessor-face ((t (:foreground "#cd5555" :weight normal))))
+ '(font-lock-warning-face ((t (:foreground "#FF0000" :weight bold))))
+
+ ;; org mode
  '(org-level-1 ((t (:foreground "#c1cdcd" :weight bold))))
  '(org-level-2 ((t (:foreground "#8b8378"))))
+
+ ;; highlight
+ '(highlight ((t (:background "#4a708b"))))
+ '(region ((t (:background "#cdb7b5"))))
+
  ;; company
  '(company-preview ((t (:foreground "darkgray" :underline t))))
  '(company-preview-common ((t (:inherit company-preview))))
@@ -29,6 +37,7 @@
  '(company-scrollbar-bg ((t (:background "#121212"))))
  '(company-scrollbar-fg ((t (:background "#121212"))))
  '(eldoc-highlight-function-argument ((t (:inherit bold :foreground "#00ee76" :underline t :weight bold))))
+ 
  ;; whitespace-mode for 80 columns
  '(whitespace-line ((t (:foreground "IndianRed4"))))
  ;; show-paren-mode
