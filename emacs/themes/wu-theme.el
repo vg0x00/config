dc7ea4c8153251bc8aa8c8e4965d66a140b0fae5
vg0x00 <vg0x00@gmail.com>
2017-11-19 14:24:17 +0800

    update default foreground color to wheat4

--- emacs/themes/wu-theme.el
+++ emacs/themes/wu-theme.el
@@ -2,7 +2,7 @@
 
 (custom-theme-set-faces
  'wu
- '(default ((t (:family "Fira Code" :background "#121212" :foreground "#93a1a1"))))
+ '(default ((t (:family "Fira Code" :background "#121212" :foreground "wheat4"))))
  
  ;; build in syntax
  '(font-lock-builtin-face ((t (:foreground "honeydew4"))))
